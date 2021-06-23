24438014d38fbb81edafd4c37f641e360d409866
Nicholas Baroni <nbaroni@tuta.io>
2021-06-23 10:47:40 +0900

    Replace hyperspace with corestore and networker

--- dev-seeder/index.js
+++ dev-seeder/index.js
@@ -1,52 +1,31 @@
-import {
-  Server as HyperspaceServer,
-  Client as HyperspaceClient
-} from 'hyperspace'
+import Corestore from 'corestore'
+import Networker from '@corestore/networker'
 import Hyperdrive from 'hyperdrive'
 import fs from 'fs'
 import storage from 'random-access-memory'
 
-const HOST_ID = 'seeder'
-
-async function buildDrive (client, filePath, content) {
-  const drive = new Hyperdrive(client.corestore(), null)
-  await drive.promises.ready()
-  await drive.promises.writeFile(filePath, content)
-  return drive
-}
-
 async function main () {
-  const server = new HyperspaceServer({
-    storage,
-    host: HOST_ID
-  })
-  await server.ready()
-  console.log('HyperspaceServer is listening...')
+  const corestore = new Corestore(storage)
+  await corestore.ready()
+  const networker = new Networker(corestore)
 
-  // Print some client connection/disconnection events.
-  server.on('client-open', () => {
-    console.log('A HyperspaceClient has connected')
-  })
-  server.on('client-close', () => {
-    console.log('A HyperspaceClient has disconnected')
-  })
+  // Create a drive for a JS module
+  const jsModuleDrive = new Hyperdrive(corestore, null)
+  await jsModuleDrive.promises.ready()
+  const jsModuleContent = fs.readFileSync('/app/dev-seeder/webapp-dependency/index.js', 'utf-8')
+  await jsModuleDrive.promises.writeFile('/index.js', jsModuleContent)
+  
+  // Create a drive for a single page application that uses the JS module
+  const webappContent = fs.readFileSync('/app/dev-seeder/webapp/index.html', 'utf-8')
+  const webappDrive = new Hyperdrive(corestore, null)
+  await webappDrive.promises.ready()
+  await webappDrive.promises.writeFile('/index.html', webappContent)
+  await webappDrive.promises.mount('/jsModule', jsModuleDrive.key)
 
-  const client = new HyperspaceClient({ host: HOST_ID })
-
-  const webappDependency = fs.readFileSync('/app/dev-seeder/webapp-dependency/index.js', 'utf-8')
-  const webappDependencyDrive = await buildDrive(client, '/index.js', webappDependency)
-  const webappDependencyKey = webappDependencyDrive.key.toString('hex')
-  const webapp = fs
-    .readFileSync('/app/dev-seeder/webapp/index.html', 'utf-8')
-    .replace('{{dependencyKey}}', webappDependencyKey)
-  const webappDrive = await buildDrive(client, '/index.html', webapp)
-  const webappKey = webappDrive.key.toString('hex')
-
-  console.log('JS MODULE KEY:', webappDependencyKey)
-  console.log('WEB APP KEY:', webappKey)
+  console.log('WEB APP KEY:', webappDrive.key.toString('hex'))
 
   // Log when the core has any new peers.
-  webappDependencyDrive.on('peer-add', () => {
+  jsModuleDrive.on('peer-add', () => {
     console.log('(webappDependency) Replicating with a new peer.')
   })
 
@@ -57,16 +36,15 @@ async function main () {
 
   // seed both drives
   await Promise.all([
-    client.replicate(webappDependencyDrive.metadata),
-    client.replicate(webappDrive.metadata)
+    networker.configure(jsModuleDrive.discoveryKey, { announce: true, lookup: false }),
+    networker.configure(webappDrive.discoveryKey, { announce: true, lookup: false })
   ])
 
   process.on('SIGINT', cleanup)
 
   async function cleanup () {
     console.log('Seeder shutting down...')
-    await client.close()
-    await server.close()
+    await networker.close()
   }
 }
 
